e8bde86aea9da0b700b9032a8c406f7fd4b532a2
MrGarrison7212 <bokipfc1992@gmail.com>
2024-04-17 23:38:29 +0200

    Adding ViewModel.

--- composeApp/src/commonMain/kotlin/HomeRepository.kt
+++ composeApp/src/commonMain/kotlin/HomeRepository.kt
@@ -1,11 +1,12 @@
 import apiClient.httpClient
+import data.Product
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import kotlinx.coroutines.flow.flow
 
 class HomeRepository {
 
-    suspend fun  getProductsApi() {
+    suspend fun getProductsApi() : List<Product> {
         val response = httpClient.get("https://fakestoreapi.com/products")
         return response.body()
     }
